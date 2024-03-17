c12f5b631420cf3f915e1aaad247d0bae6afdf44
thrishank <thrishankkalluru@gmail.com>
2024-03-17 11:00:42 +0530

    removed the unwanted code

--- server/app.js
+++ server/app.js
@@ -10,12 +10,4 @@ app.use(express.json());
 
 app.use("/api/v1", router);
 
-app.get("/", (req,res) => {
-  const q = req.query;
-  res.send(q);
-})
-
-// app.get("/", (req, res) => {
-//   res.send(process.env.JWT_PASSWORD);
-// });
 app.listen(3000);
